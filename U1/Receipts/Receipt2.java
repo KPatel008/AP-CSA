87d2662eecace8cd191c555e39cb96536b3424b8
KPatel008 <krishjpatel@billericak12.com>
2025-09-06 21:40:11 -0400

    Finished receipts assignment

---
+++ U1/Receipts/Receipt2.java
@@ -0,0 +1,31 @@
+package U1.Receipts;
+/**
+ * Receipt2.java  
+ *
+ * @author – Krish Patel
+ * @author – 3B
+ */ 
+public class Receipt2 {
+   public static void main(String [] args)
+   {
+    String hsname = "Trevor Packer HS";
+    String Drink = "Drink";
+    String Candy = "Candy";
+    String Hotdog = "Hot Dog";
+    String Hamburger = "Hamburger";
+    double drink = 1.50;
+    double candy = 1.25;
+    double hotdog = 2.75;
+    double hamburger = 3.50;
+      System.out.println("**************************************");
+      System.out.println("*                                    *");
+      System.out.println("*     "+ hsname + " Snack Bar     *");
+      System.out.println("*                                    *");
+      System.out.println("*     " + Drink + "..........$" + drink +  "0           *");                      
+      System.out.println("*     " + Candy + "..........$" + candy +  "           *");    
+      System.out.println("*     " + Hotdog + "........$" + hotdog +  "           *");   
+      System.out.println("*     " + Hamburger + "......$" + hamburger +  "0           *");   
+      System.out.println("*                                    *");    
+      System.out.println("**************************************");
+   }
+}
