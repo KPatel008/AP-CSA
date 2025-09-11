243d748230c9497650d944081d0ad927ec076dc3
KPatel008 <krishjpatel@billericak12.com>
2025-09-11 09:28:12 -0400

    math

---
+++ U1/Math/MathOps.java
@@ -0,0 +1,15 @@
+package U1.Math;
+
+public class MathOps{
+    public static void main (String[] args) {
+        int x = 5;
+        int y = 8;
+        double m = 6.25;
+        System.out.println(x + " " + y + " " + m);
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println((double)x/y);
+        System.out.println("x+y is " + x + y);
+    }
+}
