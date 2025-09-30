41499fc345c5c8b4814a8954533a6e94db934ff0
KPatel008 <krishjpatel@billericak12.com>
2025-09-29 21:41:52 -0400

    Finished Complex assignment

--- U1/FrogsFractionsComplex/Complex.java
+++ U1/FrogsFractionsComplex/Complex.java
@@ -38,6 +38,35 @@ public class Complex {
     public Complex add(double a) {
         double r = this.real + a;
         double i = this.imaginary;
+
+        Complex result = new Complex(r,i);
+        return result;
+    }
+
+    public Complex multiply(double a) {
+        double r = this.real * a;
+        double i = this.imaginary * a;
+
+        Complex result = new Complex(r,i);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Complex c1 = new Complex(2,-3);
+        Complex c2 = new Complex(4,5);
+        Complex c3 = new Complex(6);
+        Complex c4 = new Complex(-7,8);
+        Complex c5 = new Complex(0,2);
+        Complex c6 = new Complex(3,0);
+
+        Complex c7 = c1.add(c2);
+        System.out.println(c7);
+        Complex c8 = c3.multiply(c4);
+        System.out.println(c8);
+        Complex c9 = c5.add(3);
+        System.out.println(c9);
+        Complex c10 = c6.multiply(4);
+        System.out.println(c10);
     }
 
 
