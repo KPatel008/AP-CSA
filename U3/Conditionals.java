085e9dcc467af3784024272d8bbe7d144601c401
KPatel008 <krishpatel@billericak12.com>
2025-10-29 10:33:42 -0400

    finished conditionals (i love stefon diggs)

--- U3/Conditionals.java
+++ U3/Conditionals.java
@@ -53,6 +53,48 @@ public class Conditionals {
         }
     }
 
+    public static boolean onesDigitSame(int a, int b, int c) {
+         a = a%10;
+         b = b%10;
+         c = c%10;
+        if (a==c||a==b||b==c) {
+            return true; }
+        else
+            return false;
+    }
+
+    public static int blackjack(int a, int b) {
+        if (a>21&&b>21) {
+            return 0;
+        }
+        if (b>21) {
+            return a;
+        }
+        if (a>21){
+            return b;
+        }
+        if (a>b) {
+            return a;
+        }  
+        else
+            return b;
+        }
+
+    public static String popBang(String str) {
+        String first = str.substring(0,1);
+        String last = str.substring(str.length()-1);
+        if (first.equals("p")&&last.equals("b")) {
+            return "PopBang";
+        }
+        if (first.equals("p")) {
+            return "Pop";
+        }
+        if (last.equals("b")) {
+            return "Bang";
+        }
+        else
+            return str;
+    }
 
     public static void main(String[] args) {
         System.out.println("freshmanTrouble");
@@ -96,5 +138,34 @@ public class Conditionals {
         String ae = setAlarm (4, true);
         String af = setAlarm (6, true);
         System.out.println(aa + " " + ab + " " + ac + " " + ad + " " + ae + " " + af);
+
+        System.out.println("");
+
+        System.out.println("onesDigitSame");
+        boolean oa = onesDigitSame(23, 19, 13);
+        boolean ob = onesDigitSame (23, 19, 12);
+        boolean oc = onesDigitSame (23, 19, 3);
+        boolean od = onesDigitSame (423, 13, 3);
+        boolean oe = onesDigitSame (23, 29, 25);
+        System.out.println(oa + " " + ob + " " + oc + " " + od + " " + oe);
+
+        System.out.println("");
+
+        System.out.println("blackjack");
+        int ba = blackjack(19, 21);
+        int bb = blackjack(21, 19);
+        int bc = blackjack(19, 22);
+        int bd = blackjack(8, 8);
+        int be = blackjack(25, 24);
+        int bf = blackjack(17, 9);
+        int bg = blackjack(12,18);
+        System.out.println(ba + " " + bb + " " + bc + " " + bd + " " + be + " " + bf + " " + bg);
+
+
+        String p2a = popBang("party");
+        String p2b = popBang("glob");
+        String p2c = popBang("plumb"); 
+        String p2d = popBang("hello"); 
+        System.out.println(p2a + " " + p2b + " " + p2c + " " + p2d);
     }
 }
