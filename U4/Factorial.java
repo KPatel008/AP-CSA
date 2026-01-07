7a326727ff69f8b2abd73c9c7aa1bd00e9a1d44b
KPatel008 <krishjpatel@billericak12.com>
2026-01-07 11:55:19 -0500

    h

--- U4/Factorial.java
+++ U4/Factorial.java
@@ -22,11 +22,9 @@ public class Factorial {
     public static double calcEX(int x) {
         double e = 1 + Math.pow(x,2)/calcFactorial(2) + x/calcFactorial(1);
         double eold = 1 + x/calcFactorial(1);
-        int a = 3;
-        while (e - eold >= 0.001) {
-            e += Math.pow(x, a)/calcFactorial(a);
-            e += Math.pow(x, a-1)/calcFactorial(a-1);
-            a++;
+        for (int i = 3; e-eold >= 0.001; i++) {
+            e += Math.pow(x,i)/calcFactorial(i);
+            eold += Math.pow(x,i-1)/calcFactorial(i-1);
         }
         return e;
     }
@@ -34,11 +32,40 @@ public class Factorial {
     public static void main(String[] args) {
         for (int k = 1; k <= 20; k++) {
             System.out.println(k + "! = " + calcFactorial(k));
-            System.out.printf("e is %2.3f \n", calcEX(3));
+        }
+
+        for (int k = 1; k <= 5; k++) {
+            System.out.printf("e is %2.3f \n", calcEX(k));
         }
 
         System.out.println(calcE());
 
-        
     }
 }
+
+// 1! = 1
+// 2! = 2
+// 3! = 6
+// 4! = 24
+// 5! = 120
+// 6! = 720
+// 7! = 5040
+// 8! = 40320
+// 9! = 362880
+// 10! = 3628800
+// 11! = 39916800
+// 12! = 479001600
+// 13! = 6227020800
+// 14! = 87178291200
+// 15! = 1307674368000
+// 16! = 20922789888000
+// 17! = 355687428096000
+// 18! = 6402373705728000
+// 19! = 121645100408832000
+// 20! = 2432902008176640000
+// e is 2.718 
+// e is 7.389 
+// e is 20.085 
+// e is 54.598 
+// e is 148.413 
+// 2.7182539682539684
